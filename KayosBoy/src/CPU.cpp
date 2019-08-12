ada9d4b701b36d7f1d6f10ff94804d009822844b
Aidan <aidan@pixelblimp.co.uk>
2019-08-12 22:32:56 +0100

    Added stubs for most of the gameboys instructions. The big exceptions being bit instructions and the LD instructions.

--- KayosBoy/src/CPU.cpp
+++ KayosBoy/src/CPU.cpp
@@ -9,3 +9,342 @@ CPU::CPU() :
 
 }
 
+// ADC
+void CPU::ADC()
+{
+
+}
+
+void CPU::ADC(uint8_t val)
+{
+
+}
+
+void CPU::ADC(ByteRegister& val)
+{
+
+}
+
+void CPU::ADC(KayosBoyPtr& ptrToVal)
+{
+
+}
+
+// ADD
+void CPU::ADD()
+{
+
+}
+
+void CPU::ADD_SP()
+{
+
+}
+
+void CPU::ADD(uint8_t val)
+{
+
+}
+
+void CPU::ADD(uint16_t val)
+{
+
+}
+
+void CPU::ADD(ByteRegister& val)
+{
+
+}
+
+void CPU::ADD(TwoByteRegister& val)
+{
+
+}
+
+void CPU::ADD(PairedByteRegister& val)
+{
+
+}
+
+void CPU::ADD(KayosBoyPtr& ptrToVal)
+{
+
+}
+
+// AND
+void CPU::AND()
+{
+
+}
+
+void CPU::AND(uint8_t val)
+{
+
+}
+
+void CPU::AND(ByteRegister& val)
+{
+
+}
+
+void CPU::AND(KayosBoyPtr& ptrToVal)
+{
+
+}
+
+// CALL
+void CPU::CALL()
+{
+
+}
+
+void CPU::CALL(uint8_t flagCondition)
+{
+
+}
+
+// CFF
+void CPU::CFF()
+{
+
+}
+
+// CPL
+void CPU::CPL()
+{
+
+}
+
+// DAA
+void CPU::DAA()
+{
+
+}
+
+// DEC
+void CPU::DEC(ByteRegister& val)
+{
+
+}
+
+void CPU::DEC(TwoByteRegister& val)
+{
+
+}
+
+void CPU::DEC(PairedByteRegister& val)
+{
+
+}
+
+void CPU::DEC(KayosBoyPtr& ptrToVal)
+{
+
+}
+
+// DI
+void CPU::DI()
+{
+
+}
+
+// EI
+void CPU::EI()
+{
+
+}
+
+// CP
+void CPU::CP()
+{
+
+}
+
+void CPU::CP(uint8_t val)
+{
+
+}
+
+void CPU::CP(ByteRegister& val)
+{
+
+}
+
+void CPU::CP(KayosBoyPtr& ptrToVal)
+{
+
+}
+
+// HALT
+void CPU::HALT()
+{
+
+}
+
+// INC
+void CPU::INC(ByteRegister& val)
+{
+
+}
+
+void CPU::INC(TwoByteRegister& val)
+{
+
+}
+
+void CPU::INC(PairedByteRegister& val)
+{
+
+}
+
+void CPU::INC(KayosBoyPtr& ptrToVal)
+{
+
+}
+
+// JP
+void CPU::JP()
+{
+
+}
+
+void CPU::JP(uint8_t flagCondition)
+{
+
+}
+
+void CPU::JP(KayosBoyPtr& ptr)
+{
+
+}
+
+// JR
+void CPU::JR()
+{
+
+}
+
+void CPU::JR(uint8_t flagCondition)
+{
+
+}
+
+// NOP
+void CPU::NOP()
+{
+
+}
+
+// OR
+void CPU::OR()
+{
+
+}
+
+void CPU::OR(uint8_t val)
+{
+
+}
+
+void CPU::OR(ByteRegister& val)
+{
+
+}
+
+void CPU::OR(KayosBoyPtr& ptrToVal)
+{
+
+}
+
+// POP
+void CPU::POP(PairedByteRegister& val)
+{
+
+}
+
+// PUSH
+void CPU::PUSH(PairedByteRegister& val)
+{
+
+}
+
+// RST
+void CPU::RST(uint8_t offset)
+{
+
+}
+
+// SCF
+void CPU::SCF()
+{
+
+}
+
+// STOP
+void CPU::STOP()
+{
+
+}
+
+// SUB
+void CPU::SUB()
+{
+
+}
+
+void CPU::SUB(uint8_t val)
+{
+
+}
+
+void CPU::SUB(ByteRegister& val)
+{
+
+}
+
+void CPU::SUB(KayosBoyPtr& ptrToVal)
+{
+
+}
+
+// SBC
+void CPU::SBC()
+{
+
+}
+
+void CPU::SBC(uint8_t val)
+{
+
+}
+
+void CPU::SBC(ByteRegister& val)
+{
+
+}
+
+void CPU::SBC(KayosBoyPtr& ptrToVal)
+{
+
+}
+
+// XOR
+void CPU::XOR()
+{
+
+}
+
+void CPU::XOR(uint8_t val)
+{
+
+}
+
+void CPU::XOR(ByteRegister& val)
+{
+
+}
+
+void CPU::XOR(KayosBoyPtr& ptrToVal)
+{
+
+}
--- KayosBoy/src/CPU.hpp
+++ KayosBoy/src/CPU.hpp
@@ -32,6 +32,119 @@ protected:
 	TwoByteRegister mStackPointer;
 	TwoByteRegister mProgramCounter;
 
+	// Instructions
+	// ADC
+	void ADC(); // ADC value pointed to from PC register.
+	void ADC(uint8_t val);
+	void ADC(ByteRegister& val);
+	void ADC(KayosBoyPtr& ptrToVal);
+
+	// ADD
+	void ADD(); // ADD value pointed to from PC register.
+	void ADD_SP(); // ADD value pointed to from PC register to SP.
+	void ADD(uint8_t val);
+	void ADD(uint16_t val);
+	void ADD(ByteRegister& val);
+	void ADD(TwoByteRegister& val);
+	void ADD(PairedByteRegister& val);
+	void ADD(KayosBoyPtr& ptrToVal);
+
+	// AND
+	void AND(); // AND value pointed to from PC register.
+	void AND(uint8_t val);
+	void AND(ByteRegister& val);
+	void AND(KayosBoyPtr& ptrToVal);
+
+	// CALL
+	void CALL();
+	void CALL(uint8_t flagCondition);
+
+	// CFF
+	void CFF();
+
+	// CPL
+	void CPL();
+
+	// DAA
+	void DAA();
+
+	// DEC
+	void DEC(ByteRegister& val);
+	void DEC(TwoByteRegister& val);
+	void DEC(PairedByteRegister& val);
+	void DEC(KayosBoyPtr& ptrToVal);
+
+	// DI
+	void DI(); // Disable Interrupts
+
+	// EI
+	void EI(); // Enable Interrupts
+
+	// CP
+	void CP(); // CP value pointed to from PC register.
+	void CP(uint8_t val);
+	void CP(ByteRegister& val);
+	void CP(KayosBoyPtr& ptrToVal);
+
+	// HALT
+	void HALT();
+
+	// INC
+	void INC(ByteRegister& val);
+	void INC(TwoByteRegister& val);
+	void INC(PairedByteRegister& val);
+	void INC(KayosBoyPtr& ptrToVal);
+
+	// JP
+	void JP();
+	void JP(uint8_t flagCondition); // Conditions could be handled via enums. Right now it's being handled by bit comparisons.
+	void JP(KayosBoyPtr& ptr);
+
+	// JR
+	void JR();
+	void JR(uint8_t flagCondition); // Conditions could be handled via enums. Right now it's being handled by bit comparisons.
+
+	// NOP
+	void NOP();
+
+	// OR
+	void OR(); // OR value pointed to from PC register.
+	void OR(uint8_t val);
+	void OR(ByteRegister& val);
+	void OR(KayosBoyPtr& ptrToVal);
+
+	// POP
+	void POP(PairedByteRegister& val);
+
+	// PUSH
+	void PUSH(PairedByteRegister& val);
+
+	// RST
+	void RST(uint8_t offset);
+
+	// SCF
+	void SCF();
+
+	// STOP
+	void STOP();
+
+	// SUB
+	void SUB(); // SUB value pointed to from PC register.
+	void SUB(uint8_t val);
+	void SUB(ByteRegister& val);
+	void SUB(KayosBoyPtr& ptrToVal);
+
+	// SBC
+	void SBC(); // SBC value pointed to from PC register.
+	void SBC(uint8_t val);
+	void SBC(ByteRegister& val);
+	void SBC(KayosBoyPtr& ptrToVal);
+
+	// XOR
+	void XOR(); // XOR value pointed to from PC register.
+	void XOR(uint8_t val);
+	void XOR(ByteRegister& val);
+	void XOR(KayosBoyPtr& ptrToVal);
 };
 
 #endif // _CPU_H_
